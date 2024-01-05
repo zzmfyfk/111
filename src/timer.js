5ccbd524f86c3dafaf1c13633852835379810cf6
liafeng09 <154075803+liafeng09@users.noreply.github.com>
2024-01-05 04:13:19 +0100

    timer

--- src/timer.js
+++ src/timer.js
@@ -1,43 +1,53 @@
-//Provo a fare il timer 
 function create_timer(s, player) {
-
-    PP.timers.add_timer(s, 120000, time_over, false)
-
-    // setto variabile globale time a 60000
+    PP.timers.add_timer(s, 120000, time_over, false);
     PP.gameState.set_variable("Time", 120000);
-    // aggiunto testo timer 
-    txt_time = PP.shapes.text_styled_add(s, 1100, 10, "Time: 120", 30, "Helvetica", "normal", "0x000000", null, 0, 0);
 
-    //fisso la scritta in alto in modo che rimanga ferma
+    let txt_time = PP.shapes.text_styled_add(s, 1100, 10, "Time: 120", 30, "Helvetica", "normal", "0xFFFFFF", null, 0, 0);
     txt_time.tile_geometry.scroll_factor_x = 0;
     txt_time.tile_geometry.scroll_factor_y = 0;
 
-    //una volta scaduto il tempo, esce la schermata di game over (successivamente andrà sostituita con la schermata della tavola di finale negativo)
     function time_over(s) {
         if (PP.timers.getTime(s) > 120000) {
-            console.log("tempo scaduto")
+            console.log("tempo scaduto");
             PP.scenes.start("game_over");
         }
     }
 
-    //creo un nuovo timer attivo ogni secondo per fare il count down
+    let isZoomed = false;
+    PP.timers.add_timer(s, 1000, function_to_call_2, true);
 
-    PP.timers.add_timer(s, 1000, function_to_call_2, true) //metto valore boleano true perchè deve ripetersi continuamente
     function function_to_call_2(s) {
-        //prendo tempo attuale
         let prev_time = PP.gameState.get_variable("Time");
+        PP.gameState.set_variable("Time", prev_time - 1000);
 
-        // setto la variabile time a prev_time - 1000
-        PP.gameState.set_variable("Time", prev_time-1000);
+        let remainingTime = PP.gameState.get_variable("Time") / 1000;
 
-        //console.log(PP.gameState.get_Variable("Time"))
+        if (remainingTime <= 15 && !isZoomed) {
+            isZoomed = true;
+            txt_time.ph_obj.setColor('#CC0000');
 
-        // aggiorno la scritta
-        PP.shapes.text_change(txt_time, "Time: " + (PP.gameState.get_variable("Time")/1000));
+            s.tweens.add({
+                targets: txt_time.ph_obj,
+                scaleX: 2,
+                scaleY: 2,
+                x: s.cameras.main.centerX,
+                y: s.cameras.main.centerY,
+                ease: 'Power1',
+                duration: 2000,
+                onComplete: function() {
+                    s.tweens.add({
+                        targets: txt_time.ph_obj,
+                        scaleX: 1,
+                        scaleY: 1,
+                        x: 1100,
+                        y: 10,
+                        ease: 'Power1',
+                        duration: 1000
+                    });
+                }
+            });
+        }
 
+        PP.shapes.text_change(txt_time, "Time: " + remainingTime);
     }
-
 }
-
-
-
