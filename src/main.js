4e772338197842881437b6ffdaffea9c244dfb9c
gaiascherini <152877746+gaiascherini@users.noreply.github.com>
2024-01-05 16:33:40 +0100

    sistemato dimensioni canvas e aggiunto un paio di tavole di prova

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 
 var config = {
     canvas_width: 1280,
-    canvas_height: 800,
+    canvas_height: 720,
     canvas_id: "game_area", // Specifica il div contenitore
     background_color: 0x000000,
     debug_mode: true,
