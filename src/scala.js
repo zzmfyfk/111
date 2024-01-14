819087f50c4b27dc4ebe9a6f8a6ffc998f2c8534
gaiascherini <gaia.scherini@mail.polimi.it>
2024-01-14 18:49:11 +0100

    scale

--- src/scala.js
+++ src/scala.js
@@ -9,17 +9,27 @@ function create_scala(s, player){
     PP.physics.add_overlap_f(s, player, scala_2, overlap_scala_1);
 
   
-    let scala_5 = PP.shapes.rectangle_add(s, 4704, 997, 132, 300,"0x000000", 0); // scala 2 che va sotto
+    let scala_5 = PP.shapes.rectangle_add(s, 4704, 997, 132, 280,"0x000000", 0); // scala 2 che va sotto
     PP.physics.add(s, scala_5, PP.physics.type.STATIC);
     PP.physics.add_overlap_f(s, player, scala_5, overlap_scala_1);
  
 
+    let end_scale_2=PP.shapes.rectangle_add(s, 1720, 815, 100 , 10,"0x000000", 0);
+    PP.physics.add(s, end_scale_2, PP.physics.type.STATIC);
+    PP.physics.add_overlap_f(s, player, end_scale_2, overlap_fine_scala_2);      //fine superiore della prima scala discendente
 
+    let end_scale_5=PP.shapes.rectangle_add(s, 4704, 715, 124 , 10,"0x000000", 0);
+    PP.physics.add(s, end_scale_5, PP.physics.type.STATIC);
+    PP.physics.add_overlap_f(s, player, end_scale_5, overlap_fine_scala_2);      //fine superiore della seconda scala discendente
 
 
    function overlap_scala_1(s, player, scala_1) {
    player.is_on_scala = true;
    }
+
+   function overlap_fine_scala_2(s, player) {
+    player.is_on_fine_scala_2 = true;
+}
 
 }
 
--- src/scala_pioli.js
+++ src/scala_pioli.js
@@ -11,19 +11,25 @@ function create_scala_pioli(s, player){
     PP.physics.add_overlap_f(s, player, end_scale, overlap_fine_scala);      //fine scala superiore
 
    
-    let scala_3 = PP.shapes.rectangle_add(s, 3760, 720, 10, 355,"0x000000", 0);//scala 2 tetto 
+    let scala_3 = PP.shapes.rectangle_add(s, 3760, 720, 10, 358,"0x000000", 0);//scala 2 tetto 
     PP.physics.add(s, scala_3, PP.physics.type.STATIC);
     PP.physics.add_overlap_f(s, player, scala_3, overlap_scala_1);
 
-    let scala_4 = PP.shapes.rectangle_add(s, 3760, 720, 10, 330,"0x000000", 0); //scala 2 tetto 
-    PP.physics.add(s, scala_4, PP.physics.type.STATIC);
-    PP.physics.add_overlap_f(s, player, scala_4, overlap_scala_1);
+    let end_scale_3=PP.shapes.rectangle_add(s, 3760, 430, 40 , 40,"0x000000", 0);
+    PP.physics.add(s, end_scale_3, PP.physics.type.STATIC);
+    PP.physics.add_overlap_f(s, player, end_scale_3, overlap_fine_scala);      //fine scala superiore
+
+
     
   
  
     let scala_6 = PP.shapes.rectangle_add(s, 5370, 300, 12, 300,"0x000000", 0); //scala 3 ed 4
     PP.physics.add(s, scala_6, PP.physics.type.STATIC);
     PP.physics.add_overlap_f(s, player, scala_6, overlap_scala_1);
+
+    let end_scale_6=PP.shapes.rectangle_add(s, 5370, 10, 40 , 40,"0x000000", 0);
+    PP.physics.add(s, end_scale_6, PP.physics.type.STATIC);
+    PP.physics.add_overlap_f(s, player, end_scale_6, overlap_fine_scala);      //fine scala superiore
     
 
     function overlap_scala_1(s, player, scala_1) {
