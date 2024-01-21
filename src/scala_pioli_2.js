1938a25cd2e659c70f56da0b19529fbad2272941
gaiascherini <gaia.scherini@mail.polimi.it>
2024-01-21 01:40:58 +0100

    posizionamenti

--- src/scala_pioli_2.js
+++ src/scala_pioli_2.js
@@ -1,9 +1,9 @@
 function create_scala_pioli_2(s, player){
-    let scala_1 = PP.shapes.rectangle_add(s, 7600, 1200, 10 , 345,"0x000000", 0); //scala 1 tetto
+    let scala_1 = PP.shapes.rectangle_add(s, 7600, 1190, 10 , 485,"0x000000", 0); //scala 1 tetto
     PP.physics.add(s, scala_1, PP.physics.type.STATIC);
     PP.physics.add_overlap_f(s, player, scala_1, overlap_scala_1);
 
-    let end_scale=PP.shapes.rectangle_add(s, 7600, 900, 40 , 40,"0x000000", 0);
+    let end_scale=PP.shapes.rectangle_add(s, 7600, 850, 40 , 10,"0x000000", 0);
     PP.physics.add(s, end_scale, PP.physics.type.STATIC);
     PP.physics.add_overlap_f(s, player, end_scale, overlap_fine_scala);      //fine scala superiore
     
