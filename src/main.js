263f218b72748a44025020257c3c47ba7b75972e
leeardor <li.ziwen.5201@gmail.com>
2024-01-21 11:53:20 +0100

    aggiorno debug mode false

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ WebFont.load({
             canvas_height: 720,
             canvas_id: "game_area", // Specifica il div contenitore
             background_color: 0x000000,
-            debug_mode: true,
+            debug_mode: false,
             gravity_value: 300,
         };
 
