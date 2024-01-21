e19f22bb94f6864004b0655f56c8b3d72ff7e7b7
gaiascherini <gaia.scherini@mail.polimi.it>
2024-01-21 03:00:10 +0100

    posizione giada

--- src/giada.js
+++ src/giada.js
@@ -85,7 +85,7 @@ function create_giada(s, player) {
         PP.assets.sprite.animation_add( giada_tetto1, "light", 0,3,4,-1);
         PP.assets.sprite.animation_play( giada_tetto1, "light");
 
-        giada_ponte = PP.assets.sprite.add(s,sprite_giada,  2817, 516, 0.5, 1);
+        giada_ponte = PP.assets.sprite.add(s,sprite_giada,  3140, 490, 0.5, 1);
         PP.physics.add(s,  giada_ponte, PP.physics.type.STATIC);
         PP.physics.set_collision_rectangle( giada_ponte, 21, 27, 39, 36); 
         PP.physics.add_overlap_f(s, player,  giada_ponte, collision_giada);
